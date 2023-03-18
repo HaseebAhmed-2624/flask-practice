10cbc2f1e570481464def2cb5bcadf3e6b1b7c8c
haseebahmed-deutics <haseeb.ahmed@deutics.com>
2023-03-19 01:12:32 +0500

    auth app flask

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+# from flask import Flask
+#
+# app = Flask(__name__)
+# from flaskr import create_app
+#
+#
+#
+# if __name__ == '__main__':
+#     app = create_app()
+#     app.run(debug=True)
